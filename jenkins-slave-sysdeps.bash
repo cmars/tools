6da188dc1c7acbb1d6727a584ce8267f8f0d4e81
Casey Marshall <casey.marshall@canonical.com>
2016-03-30 14:42:53 -0500

    Also need charm-tools.

--- jenkins-slave-sysdeps.bash
+++ jenkins-slave-sysdeps.bash
@@ -5,20 +5,22 @@ HERE=$(cd $(dirname "$0"); pwd)
 ${HERE}/install.bash
 . ${HOME}/.bash_profile
 
-sudo apt-get update -y         
-sudo apt-get install -y openjdk-7-jre-headless build-essential nacl-tools jq git bzr mercurial 
+sudo apt-get update -y
+sudo apt-get install -y openjdk-7-jre-headless build-essential nacl-tools jq git bzr mercurial
 
 sudo apt-add-repository -y ppa:ubuntu-lxc/lxd-stable
-sudo apt-get update -y         
-sudo apt-get install -y lxd
-    
-sudo gpasswd -a ubuntu lxd     
-    
+sudo apt-add-repository -y ppa:juju/devel
+sudo apt-add-repository -y ppa:juju/stable
+sudo apt-get update -y
+sudo apt-get install -y lxd charm charm-tools
+
+sudo gpasswd -a ubuntu lxd
+
 export GOPATH=$HOME/gopath
 go get -d -t github.com/juju/juju
 
 pushd $GOPATH/src/github.com/juju/juju
-git checkout juju-2.0-beta3    
+git checkout juju-2.0-beta3
 godeps -u dependencies.tsv
 GOBIN=$HOME/tools/bin make install
 popd
