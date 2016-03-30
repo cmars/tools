de60914d3fe0d15a3522daa2fbce0ae2d63ed61a
Casey Marshall <casey.marshall@canonical.com>
2016-03-30 13:14:03 -0500

    Add jenkins slave sysdeps setup script.

---
+++ jenkins-slave-sysdeps.bash
@@ -0,0 +1,25 @@
+#!/bin/bash -xe
+
+HERE=PREFIX=$(cd $(dirname "$0"); pwd)
+
+${HERE}/install.bash
+. ${HOME}/.bash_profile
+
+sudo apt-get update -y         
+sudo apt-get install -y openjdk-7-jre-headless build-essential nacl-tools jq git bzr mercurial 
+
+sudo apt-add-repository -y ppa:ubuntu-lxc/lxd-stable
+sudo apt-get update -y         
+sudo apt-get install -y lxd
+    
+sudo gpasswd -a ubuntu lxd     
+    
+export GOPATH=$HOME/gopath
+go get -d -t github.com/juju/juju
+
+pushd $GOPATH/src/github.com/juju/juju
+git checkout juju-2.0-beta3    
+godeps -u dependencies.tsv
+GOBIN=$HOME/tools/bin make install
+popd
+
