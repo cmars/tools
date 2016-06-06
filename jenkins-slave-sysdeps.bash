ea036aa8e3bb3b7ee05a5b920c6117c1aae89d7a
Casey Marshall <casey.marshall@canonical.com>
2016-06-06 15:51:10 -0500

    Install juju as part of sysdeps.

--- jenkins-slave-sysdeps.bash
+++ jenkins-slave-sysdeps.bash
@@ -12,16 +12,6 @@ sudo apt-add-repository -y ppa:ubuntu-lxc/lxd-stable
 sudo apt-add-repository -y ppa:juju/devel
 sudo apt-add-repository -y ppa:juju/stable
 sudo apt-get update -y
-sudo apt-get install -y lxd charm charm-tools
+sudo apt-get install -y lxd charm charm-tools juju
 
 sudo gpasswd -a ubuntu lxd
-
-export GOPATH=$HOME/gopath
-go get -d -t github.com/juju/juju
-
-pushd $GOPATH/src/github.com/juju/juju
-git checkout juju-2.0-beta3
-godeps -u dependencies.tsv
-GOBIN=$HOME/tools/bin make install
-popd
-
